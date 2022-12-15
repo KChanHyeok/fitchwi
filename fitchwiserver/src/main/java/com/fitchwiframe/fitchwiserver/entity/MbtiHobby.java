5deeb2c11881ae9e6e4d12bc7d47bc7944374617
subeeeen <115854757+subeeeen@users.noreply.github.com>
2022-12-15 15:22:34 +0900

    신고, 팔로우 추가

---
+++ fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/entity/MbtiHobby.java
@@ -0,0 +1,23 @@
+package com.fitchwiframe.fitchwiserver.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "mbti_hobby")
+public class MbtiHobby {
+    @Id
+    private long mbtiHobbyCode;
+
+    @Column(columnDefinition = "char(4) not null")
+    private Character mbtiHobbyMbti;
+
+
+    @Column(nullable = false, length = 20)
+    private String mbtiHobbyHobby;
+}
