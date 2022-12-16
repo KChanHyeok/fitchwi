5d6597a0348a5f7c63dc35f7f261091a1956bdc9
KChanHyeok <kang9kang9@gmail.com>
2022-12-16 11:30:23 +0900

    일부 추가

--- fitchwifront/src/components/JoinMember.jsx
+++ fitchwifront/src/components/JoinMember.jsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React,{useState, useCallback} from "react";
 
 const JoinMember = () => {
+    const [joinForm, setJoinForm] = useState({
+        memail: "",
+        mpwd: "",
+        mname: "",
+        mnickname: "",
+        mgender: "",
+        mphone: "",
+        maddr: "",
+        mbirth: "",
+        mimg: "",
+        mmbti: "",
+        minterest: []
+    })
+    const onChange = useCallback(
+        (e) => {
+            const joinObj = {
+                ...joinForm,
+                [e.target.name]: e.target.value,
+            };
+            setJoinForm(joinObj);
+        }, [joinForm])
+    
+    const onCheck = useCallback(
+        (e) => {
+            const joinObj = {
+                ...joinForm,
+            };
+            if (e.target.checked === true) {
+                joinForm.minterest.push(e.target.value);
+            } else {
+                    joinForm.minterest.splice(joinForm.minterest.indexOf(e.target.value),1);
+            }
+            setJoinForm(joinObj);
+        }, [joinForm])
+ 
+    const sendJoin = (e) => {
+        e.preventDefault();
+        console.log(joinForm)
+    }
+
+
     return (
         <div>
-            <h1>여기는 회원가입페이지</h1>
+            <h1>여기는 회원가입 페이지</h1>
+            <form className="joinContainer" onSubmit={sendJoin}>
+                <div> 이메일 : <input type="email" name="memail" onChange={onChange}/></div>
+                <div> 비밀번호 : <input type="password" name="mpwd" onChange={onChange}/></div>
+                <div> 이름 : <input type="text" name="mname" onChange={onChange}/></div>
+                <div> 닉네임 : <input type="text" name="mnickname" onChange={onChange}/></div>
+                <div> 성별 : 남 <input type="radio" name="mgender" value="남" onChange={onChange}/> 여<input type="radio" name="mgender" value="여" onChange={onChange}/></div>
+                <div> 핸드폰번호 : <input type="text" name="mphone" onChange={onChange}/></div>
+                <div> 주소 : <input type="text" name="maddr" onChange={onChange}/></div>
+                <div> 생일 : <input type="date" name="mbirth" onChange={onChange}/></div>
+                <div> 프로필 이미지 : <input type="file" name="mimg" onChange={onChange}/></div> 
+                <div> MBTI : <input type="text" name="mmbti" onChange={onChange}/></div>
+                <div>
+                    관심 주제 :
+                    <span>
+                        <input type="checkbox" name="minterest" value="문화∙예술" onChange={onCheck} />문화∙예술
+                        <input type="checkbox" name="minterest" value="운동∙액티비티" onChange={onCheck} />운동∙액티비티
+                        <input type="checkbox" name="minterest" value="요리∙음식" onChange={onCheck} />요리∙음식
+                        <input type="checkbox" name="minterest" value="여행" onChange={onCheck} />여행
+                        <input type="checkbox" name="minterest" value="성장∙자기계발" onChange={onCheck} />성장∙자기계발
+                        <input type="checkbox" name="minterest" value="공예∙수공예" onChange={onCheck} />공예∙수공예
+                        <input type="checkbox" name="minterest" value="게임∙오락" onChange={onCheck} />게임∙오락
+                        <input type="checkbox" name="minterest" value="기타" onChange={onCheck} />기타
+                    </span>
+                </div> 
+                <button type="submit">회원가입</button>
+            </form>
         </div>
     );
 };
