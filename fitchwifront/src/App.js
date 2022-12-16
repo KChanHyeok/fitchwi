3bd37b42d684b06c78d1fa0a75ab9a99455e206f
subeeeen <115854757+subeeeen@users.noreply.github.com>
2022-12-16 15:19:45 +0900

    얘기해요 개설창 구현중

--- fitchwifront/src/App.js
+++ fitchwifront/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Feed from "./pages/feed";
+import Talk from "./pages/talk";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/feed" element={<Feed />}></Route>
+        <Route path="/talk" element={<Talk />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fitchwifront/src/pages/talk/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TalkOpened from "./components/TalkOpened";
+
+function index() {
+    return (
+        <TalkOpened />
+    );
+}
+
+export default index;
