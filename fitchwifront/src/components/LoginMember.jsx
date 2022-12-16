0cb360489382762439cdb429b4c4f10d65edf9de
KChanHyeok <kang9kang9@gmail.com>
2022-12-16 15:27:46 +0900

    로그인 데이터 전달 작업 완료

--- fitchwifront/src/components/LoginMember.jsx
+++ fitchwifront/src/components/LoginMember.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
 
 const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    memberEmail:"",
+    memberPwd: ""
+  })
+
+  const {memberEmail, memberPwd} = loginForm;
+
+  const onLoginChange = useCallback((e) => {
+    const loginObj = {
+      ...loginForm,
+      [e.target.name]: e.target.value
+    }
+    setLoginForm(loginObj);
+  },[loginForm])
+
+  const onLoginSend = useCallback((e) => {
+    e.preventDefault();
+    console.log(loginForm)
+  },[loginForm])  
+
   return (
-    <form>
+    <form onSubmit={onLoginSend}>
       여기는 로그인 페이지
-      <div> 아이디 : <input type="text"/></div>
-      <div> 비밀번호 : <input type="password"/></div>
+      <div> 아이디 : <input type="text" name="memberEmail" onChange={onLoginChange} value={memberEmail}/></div>
+      <div> 비밀번호 : <input type="password" name="memberPwd" onChange={onLoginChange} value={memberPwd}/></div>
       <button type="submit">로그인</button>
     </form>
   );
