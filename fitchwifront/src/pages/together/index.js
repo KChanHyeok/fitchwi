3c850cd9691f228360726976091e84a84038718a
KChanHyeok <kang9kang9@gmail.com>
2022-12-20 18:24:41 +0900

    일부분 정리

--- fitchwifront/src/pages/together/index.js
+++ fitchwifront/src/pages/together/index.js
@@ -3,8 +3,16 @@ import Sidebar from "../../layout/Sidebar";
 import TogetherMain from "./components/togetherMain";
 import { Box, Stack } from "@mui/material";
 import TogetherAdd from "./components/togetherAdd";
+import axios from "axios";
+
 
 const Together = () => {
+    
+    axios.get("/getAllFeedList").then((res) => {
+        console.log(res)
+    })
+
+
     return (
         <Box>
             <Stack direction="row" spacing={7} justifyContent="space-between">
---
+++ fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/repository/FacilitiesRepository.java
@@ -0,0 +1,7 @@
+package com.fitchwiframe.fitchwiserver.repository;
+
+import com.fitchwiframe.fitchwiserver.entity.Facilities;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FacilitiesRepository extends CrudRepository<Facilities, String> {
+}
