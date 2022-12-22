8b15a624319b7631b38deb07e3d0ed6c4edb7104
KChanHyeok <kang9kang9@gmail.com>
2022-12-22 16:15:28 +0900

    기본 repository와 타입 재 정의 및 함께해요 전체 조회 추가

--- fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/repository/TogetherOpenedRepository.java
+++ fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/repository/TogetherOpenedRepository.java
@@ -3,5 +3,5 @@ package com.fitchwiframe.fitchwiserver.repository;
 import com.fitchwiframe.fitchwiserver.entity.TogetherOpened;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TogetherOpenedRepository extends CrudRepository<TogetherOpened, String> {
+public interface TogetherOpenedRepository extends CrudRepository<TogetherOpened, Long> {
 }
