2ae1505c76536952e9eccf25afd463f9e0919d44
KChanHyeok <kang9kang9@gmail.com>
2022-12-26 14:53:51 +0900

    가입하기 프론트 완료

---
+++ fitchwifront/src/pages/together/components/togetherJoin.jsx
@@ -0,0 +1,89 @@
+import { Avatar, Button, Modal, styled, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+
+const TogetherJoin = ({children, togetherInfo}) => {
+    const nowdate = new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate();
+
+    const [insertForm,setInsertFrom] = useState({
+        togetherJoinDate: nowdate,
+        togetherJoinAnswer: "",
+        togetherCode: {
+            togetherCode: togetherInfo.togetherCode
+        },
+        memberEmail: {
+            memberEmail: sessionStorage.getItem("id")
+        }
+    })
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "white",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+      };
+      const UserBox = styled(Box)({
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        marginBottom: "20px",
+      });
+
+      const handleChange = (e) => {
+        const insertObj = {
+            ...insertForm,
+            [e.target.name] : e.target.value,
+        }
+        setInsertFrom(insertObj);
+      }
+
+    return (
+        <>
+            <Button onClick={handleOpen} variant={"contained"} sx={{maxWidth:900}}>
+            {children}
+            </Button>
+            <Modal
+            keepMounted
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="keep-mounted-modal-title"
+            aria-describedby="keep-mounted-modal-description"
+            >
+                <Box sx={style}>
+                    <UserBox>
+                        <Avatar alt={"profil.memberImg"} sx={{ width: 30, height: 30 }} />
+                        <Typography fontWeight={500} variant="span">
+                        {sessionStorage.getItem("id")}
+                        </Typography>
+                    </UserBox>
+                    <hr/>
+                    <Typography sx={{ mt: 2 }} variant="h6" component="div"> {/*질문*/}
+                        {togetherInfo.togetherInquiry}
+                    </Typography>
+                    <TextField
+                    value={insertForm.togetherJoinAnswer}
+                    onChange={handleChange}
+                    autoFocus
+                    margin="dense"
+                    name="togetherJoinAnswer"
+                    label="질문의 응답해주세요"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    />
+                    <Button variant="contained">참여하기</Button>
+                </Box>
+            </Modal>
+        </>
+    )
+}
+
+export default TogetherJoin;
