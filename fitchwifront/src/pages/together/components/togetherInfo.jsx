f30c1e912b8a5954ae3b91956d503dd0ee0f9beb
KChanHyeok <kang9kang9@gmail.com>
2022-12-30 16:55:52 +0900

    문제 수정입니다

--- fitchwifront/src/pages/together/components/togetherInfo.jsx
+++ fitchwifront/src/pages/together/components/togetherInfo.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, styled, Avatar, Typography, Button } from "@mui/material";
+import { Box, Stack, styled, Avatar, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import TogetherJoin from "./togetherJoin";
@@ -8,17 +8,6 @@ const TogetherInfo = ({ togetherJoinList, togetherList, refreshTogetherJoinList 
   let { togetherPageCode } = useParams();
   const [togetherInfo, setTogetherInfo] = useState(null);
   const [togetherJoinMember, setTogetherJoinMember] = useState(null);
-
-  useEffect(() => {
-    setTogetherInfo(togetherList.filter((data) => data.togetherCode === togetherPageCode * 1)[0]);
-    setTogetherJoinMember(
-      togetherJoinList.filter(
-        (data) =>
-          data.togetherCode.togetherCode === togetherPageCode * 1 &&
-          data.togetherJoinState === "가입중"
-      )
-    );
-  }, [togetherJoinList, togetherList, togetherPageCode]);
 
     useEffect(() => {
         try {
@@ -30,6 +19,13 @@ const TogetherInfo = ({ togetherJoinList, togetherList, refreshTogetherJoinList 
                 
             }
     }, [togetherJoinList, togetherList, togetherPageCode])
+
+    const UserBox = styled(Box)({
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        marginBottom: "20px",
+      });
 
 
     return (
