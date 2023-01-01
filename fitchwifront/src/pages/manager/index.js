c407e7abbbcdbdceb378adb850707bc18b6545ee
suyeop <suyeop627@gmail.com>
2023-01-02 08:58:49 +0900

    불필요한 파일 제거

--- fitchwifront/src/pages/manager/index.js
+++ fitchwifront/src/pages/manager/index.js
@@ -7,7 +7,6 @@ import Facilities from "./components/facilities/Facilities";
 import ViewFacilities from "./components/facilities/ViewFacilities";
 import Manager from "./components/Manager";
 import ManagerSideBar from "./components/ManagerSideBar";
-import ReportDetail from "./components/report/ReportDetail";
 import ReportManagement from "./components/report/ReportManagement";
 
 export default function index() {
@@ -22,7 +21,6 @@ export default function index() {
         <Route path="/facilities/updateFacilities/:facilitiesCode" element={<EditFacilities />} />
         <Route path="/facilities/getFacilitiesInfo/:facilitiesCode" element={<ViewFacilities />} />
         <Route path="/report" element={<ReportManagement />} />
-        <Route path="/report/detail/:reportCode" element={<ReportDetail />} />
       </Routes>
     </Box>
   );
