4f3f31c514190d7113365402f72493766c898c89
kang9kang9 <kang9kang9@gmail.com>
2023-01-02 23:44:41 +0900

    util.List 추가

--- fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/repository/TalkRepository.java
+++ fitchwiserver/src/main/java/com/fitchwiframe/fitchwiserver/repository/TalkRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface TalkRepository extends CrudRepository<Talk, Long> {
     @Transactional
