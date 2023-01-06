9bc101d3be15a503873628b8bafda6f12f91b6a6
subeeeen <115854757+subeeeen@users.noreply.github.com>
2023-01-06 12:40:39 +0900

    경고처리, 프론트 작업 중

--- fitchwifront/src/pages/talk/components/TalkInfo.jsx
+++ fitchwifront/src/pages/talk/components/TalkInfo.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "../styles/TalkInfo.scss";
 import TalkOpMenu from "../components/TalkOpMenu";
 import { Stack } from "@mui/system";
-import { Avatar, Box, Button, Modal, TextField, Typography } from "@mui/material";
+import { Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, TextField, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 import TalkJoin from "./TalkJoin";
+import Report from "../../../components/common/Report";
+import axios from "axios";
 
 const StyleModal = styled(Modal)({
     display: "flex",
@@ -39,27 +41,34 @@ const TalkInfo = ({ talkList, talkTagList, talkJoinList,
         } catch (e) {
 
         }
-        // console.log((talkTagList.filter(data => data.talkCode.talkCode === (talkPageCode * 1))[0]));
     }, [talkList, talkTagList, talkJoinList, talkPageCode]);
 
-    //문의하기 모달창
-    const [inquiryModal, setInquiryMoal] = useState(false);
+    //삭제하기
+    const deleteTalk = useCallback(
+        () => {
+            axios.delete("/deleteTalk", { data: talkInfo })
+                .then((res) => {
+                    if (res.data === "ok") {
+                        alert("얘기해요 삭제 완료");
+                        nav("/talk");
+                        refreshTalkList();
+                    } else {
+                        alert("삭제 불가");
+                    }
+                });
+        }, [talkInfo, nav, refreshTalkList]
+    );
 
-    const id = sessionStorage.getItem("id");
+    //삭제 모달 창
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
 
-    //로그인 조건
-    const isLogin = () => {
-        if (id === null) {
-            alert("로그인이 필요한 서비스입니다.");
-            nav("/login");
-        } else {
-            setInquiryMoal(!inquiryModal);
-        }
-    }
+    const isDelete = () => {
+        setOpenDeleteModal(true);
+    };
 
-    // console.log(talkTagList);
-    // console.log(talkTagInfo);
-
+    const modalClose = () => {
+        setOpenDeleteModal(false);
+    };
 
     return (
         <Stack
@@ -70,125 +79,112 @@ const TalkInfo = ({ talkList, talkTagList, talkJoinList,
             spacing={2}
         >
             {talkList.length === 0 || !talkInfo || !talkJoinMember ? <h1>로딩중</h1> :
-                <Box>
-                    <Box>
-                        <span>mbti 취미&nbsp;</span>
-                        <span>{talkInfo.talkCategory}&nbsp;</span>
-                        <span>남은 자리 {1 + talkJoinMember.length}/{talkInfo.talkMax}&nbsp;</span>
-                        <span>유형 - {talkInfo.talkType}</span>
-                        <Box className="talkMenu">
-                            {talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberEmail === id
-                                ? (<TalkOpMenu talkPageCode={talkPageCode} talkInfo={talkInfo} talkTagInfo={talkTagInfo}
-                                    talkJoinList={talkJoinList} talkJoinMember={talkJoinMember}
-                                    refreshTalkTagList={refreshTalkTagList} refreshTalkList={refreshTalkList} />)
-                                : (<Button id="demo-customized-button"
-                                    className="reportBtn"
-                                    aria-haspopup="true"
-                                    variant="contained"
-                                    disableElevation
-                                    onClick={isLogin}
-                                >신고하기</Button>)}
+                <Box className="talkSection">
+                    <Box className="talkDetailBox">
+                        <Box className="talkTxtLine">
+                            <span className="talkSubHeader">mbti 취미&nbsp;</span>
+                            <span className="talkSubHeader">{talkInfo.talkCategory}&nbsp;</span>
+                            <span className="talkSubHeader">남은 자리 {1 + talkJoinMember.length}/{talkInfo.talkMax}&nbsp;</span>
+                            <span className="talkSubHeader">유형 - {talkInfo.talkType}</span>
+                            <Box className="talkMenu">
+                                {talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberEmail === sessionStorage.getItem("id")
+                                    ? (<TalkOpMenu talkPageCode={talkPageCode} talkInfo={talkInfo} talkTagInfo={talkTagInfo}
+                                        talkJoinList={talkJoinList} talkJoinMember={talkJoinMember}
+                                        refreshTalkTagList={refreshTalkTagList} refreshTalkList={refreshTalkList} />)
+                                    : (<Report type="MenuItem"
+                                        target={talkInfo.talkCode}
+                                        targetMember={talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberEmail}
+                                        category="talk" />)}
+                            </Box>
                         </Box>
-                    </Box>
-                    <h1>{talkInfo.talkTitle}</h1>
-                    <Box sx={{ maxWidth: 900 }}>
-                        {talkInfo.talkSaveimg && (<Box
-                            component="img"
-                            sx={{ maxHeight: 400, textAlign: "center" }}
-                            src={`/images/${talkInfo.talkSaveimg}`}
-                            alt="talkimg"
-                        ></Box>)}
-                    </Box><h3>얘기해요 소개</h3><Box component="span">
-                        {talkInfo.talkContent}
-                        <h4>방장<br />
-                            {talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberNickname}</h4>
-                        <Avatar
-                            src={`/images/${talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberSaveimg}`}
-                            sx={{ width: 40, height: 40, mr: 2 }}
-                        />
-                        <h4>참여중인 회원</h4>
-                        {talkJoinMember.length === 0
-                            ? <Box component="span">현재 참여중인 멤버가 없습니다</Box>
-                            : talkJoinMember.map((data) =>
-                                <UserBox key={data.talkJoinCode}>
-                                    <Avatar src={`/images/${data.memberEmail.memberSaveimg}`} alt={"profil.memberImg"} sx={{ width: 30, height: 30 }} />
-                                    <Typography fontWeight={500} variant="span">
-                                        {data.memberEmail.memberNickname}님
-                                    </Typography>
-                                </UserBox>)}
-                        {!talkTagInfo ? <h4>로딩</h4>
-                            : <Box>
-                                <h4>태그</h4>
-                                {talkTagInfo.talkTagContent}
-                            </Box>}
+                        <h1 className="talkTitle talkTxtLine">{talkInfo.talkTitle}</h1>
+                        <Box sx={{ maxWidth: 900 }}>
+                            {talkInfo.talkSaveimg && (<Box
+                                component="img"
+                                sx={{ maxHeight: 400, textAlign: "center" }}
+                                src={`/images/${talkInfo.talkSaveimg}`}
+                                alt="talkimg"
+                            ></Box>)}
+                        </Box>
+                        <h3 className="talkTxtLine">얘기해요 소개</h3>
+                        <Box component="span">
+                            {talkInfo.talkContent}
+                            <h4 className="talkTxtLine">방장<br /></h4>
+                            <UserBox>
+                                <Avatar
+                                    src={`/images/${talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberSaveimg}`}
+                                    sx={{ width: 40, height: 40, mr: 2 }}
+                                />
+                                <Typography fontWeight={500} variant="span">
+                                    {talkList.filter(data => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberNickname}
+                                </Typography>
+                            </UserBox>
+                            <Typography className="talkTxtLine">
+                                <h4>참여중인 회원</h4>
+                                {talkJoinMember.length === 0
+                                    ? <Box component="span">현재 참여중인 멤버가 없습니다</Box>
+                                    : talkJoinMember.map((data) =>
+                                        <UserBox key={data.talkJoinCode}>
+                                            <Avatar src={`/images/${data.memberEmail.memberSaveimg}`} alt={"profil.memberImg"} sx={{ width: 30, height: 30 }} />
+                                            <Typography fontWeight={500} variant="span">
+                                                {data.memberEmail.memberNickname}님
+                                            </Typography>
+                                        </UserBox>)}
+                            </Typography>
+                            {!talkTagInfo ? <h4>로딩</h4>
+                                : <Box>
+                                    <h4 className="talkTxtLine">태그</h4>
+                                    {talkTagInfo.talkTagContent}
+                                </Box>}
 
-                        <Box>
+                            <Box>
+                            </Box>
+                            <h4 className="talkTxtLine">얘기해요 피드</h4>
+                            <h4 className="talkTxtLine">얘기해요 소개</h4>
+                            {talkInfo.talkContent}
                         </Box>
-                        <div>얘기해요 피드</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
-                        <div>dd</div>
+                        <Box className="talkTxtLine">
+                            {!talkJoinMember
+                                ? <h2>로딩중</h2>
+                                : talkList.filter((data) => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberEmail === sessionStorage.getItem("id")
+                                    ? (<Button className="talkSticky" onClick={isDelete}>삭제하기</Button>)
+                                    : talkJoinList.filter((data) => data.talkCode.talkCode === (talkPageCode * 1)
+                                        && data.memberEmail.memberEmail === sessionStorage.getItem("id")).length === 0
+                                        ? <TalkJoin talkInfo={talkInfo} refreshTalkJoinList={refreshTalkJoinList}
+                                            talkJoinMember={talkJoinMember}>참여하기</TalkJoin>
+                                        : (<TalkJoin talkInfo={talkInfo}
+                                            talkJoinState={talkJoinList.filter((data) => data.talkCode.talkCode === (talkPageCode * 1)
+                                                && data.memberEmail.memberEmail === sessionStorage.getItem("id"))[0].talkJoinState}
+                                            refreshTalkJoinList={refreshTalkJoinList} />)}
+                        </Box>
+                        <div>
+                            <Dialog
+                                open={openDeleteModal}
+                                onClose={modalClose}
+                                aria-labelledby="alert-dialog-title"
+                                aria-describedby="alert-dialog-description"
+                            >
+                                <DialogTitle id="alert-dialog-title">
+                                    {"얘기해요 삭제하기"}
+                                </DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText id="alert-dialog-description">
+                                        {talkJoinMember.length >= 1
+                                            && "현재 가입 중인 회원이 있습니다."}<br />
+                                        정말로 삭제하시겠습니까?
+                                    </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={deleteTalk}>삭제</Button>
+                                    <Button onClick={modalClose}>취소</Button>
+                                </DialogActions>
+                            </Dialog>
+                        </div>
                     </Box>
-                    {!talkJoinMember
-                        ? <h2>로딩중</h2>
-                        : talkList.filter((data) => data.talkCode === (talkPageCode * 1))[0].talkOpenCode.memberEmail.memberEmail === sessionStorage.getItem("id")
-                            ? (<Button className="talkSticky">삭제하기</Button>)
-                            : talkJoinList.filter((data) => data.talkCode.talkCode === (talkPageCode * 1)
-                                && data.memberEmail.memberEmail === sessionStorage.getItem("id")).length === 0
-                                ? <TalkJoin talkInfo={talkInfo} refreshTalkJoinList={refreshTalkJoinList}
-                                    talkJoinMember={talkJoinMember}>참여하기</TalkJoin>
-                                : (<TalkJoin talkInfo={talkInfo}
-                                    talkJoinState={talkJoinList.filter((data) => data.talkCode.talkCode === (talkPageCode * 1)
-                                        && data.memberEmail.memberEmail === sessionStorage.getItem("id"))[0].talkJoinState}
-                                    refreshTalkJoinList={refreshTalkJoinList} />)}
                 </Box>
             }
-            {!talkInfo || !talkTagInfo ? <h1>로딩중</h1> :
-                <StyleModal open={inquiryModal}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                    sx={{ mt: 5 }}>
 
-                    <Box conponent="form" width={400} height={400} bgcolor="white" p={3} borderRadius={5} sx={{ mt: 5, mb: 10, overflowY: "auto" }}>
-
-                        <Typography variant="h6" color="gray" textAlign="center">
-                            얘기해요 신고하기
-                            <button className="modalCloseBtn" onClick={() => setInquiryMoal(false)}>
-                                ✖
-                            </button>
-                        </Typography>
-
-                        <UserBox>
-                            <Avatar alt={"profil.memberImg"} sx={{ width: 30, height: 30 }} />
-                            <Typography fontWeight={500} variant="span">
-                                {id}
-                            </Typography>
-                        </UserBox>
-                        <hr />
-                        <Box>
-                            <h2 sx={{ mt: 3 }}>{talkInfo.talkTitle} 신고하기</h2><br />
-                            <h4>신고 사유를 선택해 주세요.</h4>
-                            <p>회원님의 소중한 의견은 FITCHWI를 더욱 안전하고 신뢰할 수 있도록 만드는데 큰 도움이 됩니다.</p>
-                            <TextField fullWidth
-                                label="신고내용"
-                                name="reportDetailContent"
-                                sx={{ mt: 3 }}
-                                multiline
-                                rows={3}
-                            // onChange={onChange}
-                            />
-                        </Box>
-                    </Box>
-                </StyleModal>
-            }
         </Stack>
-
-
     )
 
 }
