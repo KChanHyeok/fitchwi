3005224a255620be8d3a3022a205eb667622954f
suyeop <suyeop627@gmail.com>
2023-01-06 12:59:02 +0900

    함께해요 및 리다이랙트 오류 수정

--- fitchwifront/src/pages/login/components/KaKaoLoginRedirect.jsx
+++ fitchwifront/src/pages/login/components/KaKaoLoginRedirect.jsx
@@ -1,25 +1,49 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 //import {KAKAO_ADD_PROPERTIES} from "../share/kakaoAuth"
 
-const KaKaoLoginRedirect = (props) => {
+const KaKaoLoginRedirect = ({ sucLogin, ...props }) => {
+  const nav = useNavigate();
   useEffect(() => {
     let code = new URL(window.location.href).searchParams.get("code");
 
     const kakaoLogin = async () => {
       await axios.get("/login/kakao/callback?", { params: { code: code } }).then((res) => {
+        console.log(res.data);
         const { isPresent, member } = res.data;
+
         switch (isPresent) {
           case "ok":
-            console.log(res.data);
-            sessionStorage.setItem("id", member.memberEmail);
-            sessionStorage.setItem("nickName", member.memberNickname);
-            alert(member.memberNickname + "님 환영합니다.");
-            //  window.location.href = "/";
+            switch (res.data.state) {
+              case "ok":
+                console.log(res.data);
+                sessionStorage.setItem("id", res.data.memberEmail);
+                sessionStorage.setItem("nickName", res.data.memberNickname);
+                alert(res.data.memberNickname + "님 환영합니다.");
+                window.location.href = "/";
+                break;
+              case "reported":
+                alert("신고누적으로, " + res.data.memberRestriction + "까지 이용이 불가능합니다.");
+                nav("/");
+
+                break;
+              case "released":
+                alert(res.data.memberRestriction + "부로 이용 제한이 해제됐습니다.");
+                sucLogin(res.data.memberEmail, res.data.memberNickname);
+                sessionStorage.setItem("id", res.data.memberEmail);
+                sessionStorage.setItem("nickName", res.data.memberNickname);
+
+                nav("/");
+                break;
+
+              default:
+                break;
+            }
             break;
           case "no":
-            console.log(res.data);
+            console.log(member);
             alert("추가 정보 입력 페이지로 이동합니다.");
             break;
           case "fail":
@@ -34,6 +58,7 @@ const KaKaoLoginRedirect = (props) => {
       });
     };
     kakaoLogin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.history]);
 
   return (
